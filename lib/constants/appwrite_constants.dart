49a2dcae711482347db20fc09ca3e9ac0029bef8
Dante291 <gparag222@gmail.com>
2023-09-25 21:18:23 +0530

    initial commit

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseID = '65119f47d122b1d6eafc';
+  static const String projectID = '64fd5ef10466d51c8135';
+  static const String endPoint = 'http://localhost:80';
+}
