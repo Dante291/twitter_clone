f7861abd7b2576e98e6d8e8affc522c13ae2032a
Dante291 <gparag222@gmail.com>
2023-10-02 19:41:11 +0530

    selecting images from device and showing it in app

--- lib/features/tweet/create_tweet_view.dart
+++ lib/features/tweet/create_tweet_view.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/asset_constants.dart';
+import 'package:twitter_clone/core/utils.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -16,6 +20,32 @@ class CreateTweetScreen extends ConsumerStatefulWidget {
 
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
   final tweetTextController = TextEditingController();
+  int _currentImageIndex = 0;
+
+  List<File> images = [];
+  @override
+  void dispose() {
+    super.dispose();
+    tweetTextController.dispose();
+  }
+
+  void onpickImages() async {
+    final List<File> selectedImages = await pickImage();
+    if (selectedImages.isNotEmpty) {
+      images = selectedImages;
+      setState(() {});
+    }
+  }
+
+  void _removeCurrentImage() {
+    setState(() {
+      images.removeAt(_currentImageIndex);
+      if (_currentImageIndex >= images.length && _currentImageIndex > 0) {
+        _currentImageIndex--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final currentUSer = ref.watch(currentUserDetailsProvider).value;
@@ -52,24 +82,64 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                         width: 15,
                       ),
                       Expanded(
-                        child: TextField(
-                          onTapOutside: (_) {
-                            FocusManager.instance.primaryFocus?.unfocus();
-                          },
-                          controller: tweetTextController,
-                          style: const TextStyle(fontSize: 22),
-                          decoration: const InputDecoration(
-                              hintText: "What's Happening?",
-                              hintStyle: TextStyle(
-                                  color: Pallete.greyColor,
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.w600),
-                              border: InputBorder.none),
-                          maxLines: null,
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 8),
+                          child: TextField(
+                            onTapOutside: (_) {
+                              FocusManager.instance.primaryFocus?.unfocus();
+                            },
+                            controller: tweetTextController,
+                            style: const TextStyle(fontSize: 22),
+                            decoration: const InputDecoration(
+                                hintText: "What's Happening?",
+                                hintStyle: TextStyle(
+                                    color: Pallete.greyColor,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w600),
+                                border: InputBorder.none),
+                            maxLines: null,
+                          ),
                         ),
                       )
                     ],
                   ),
+                  const SizedBox(height: 20),
+                  if (images.isNotEmpty)
+                    CarouselSlider(
+                      items: images.map((file) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Stack(
+                            children: <Widget>[
+                              Image.file(file, fit: BoxFit.cover),
+                              Positioned(
+                                top: 5,
+                                right: 5,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.black.withOpacity(0.6),
+                                      borderRadius: BorderRadius.circular(50)),
+                                  child: IconButton(
+                                    icon: const Icon(Icons.close,
+                                        size: 25, color: Colors.white),
+                                    onPressed: _removeCurrentImage,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                      options: CarouselOptions(
+                          height: 400,
+                          enableInfiniteScroll: false,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              _currentImageIndex = index;
+                            });
+                          }),
+                    )
                 ],
               ),
             )),
@@ -80,9 +150,12 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0).copyWith(left: 10, right: 10),
-              child: SvgPicture.asset(
-                AssetsConstants.galleryIcon,
-                height: 35,
+              child: GestureDetector(
+                onTap: onpickImages,
+                child: SvgPicture.asset(
+                  AssetsConstants.galleryIcon,
+                  height: 35,
+                ),
               ),
             ),
             Padding(
