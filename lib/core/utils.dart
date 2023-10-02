2bc1865327e44ab684fface76d35dbc85c48444a
Dante291 <gparag222@gmail.com>
2023-10-02 12:13:48 +0530

    create tweet ui

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showsnackbar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(content)));
@@ -6,4 +9,16 @@ void showsnackbar(BuildContext context, String content) {
 
 String getnamefromemail(String email) {
   return email.split('@')[0];
+}
+
+Future<List<File>> pickImage() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final imagefiles = await picker.pickMultiImage();
+  if (imagefiles.isNotEmpty) {
+    for (final image in imagefiles) {
+      images.add(File(image.path));
+    }
+  }
+  return images;
 }
--- lib/features/tweet/create_tweet_view.dart
+++ lib/features/tweet/create_tweet_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/asset_constants.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -13,10 +15,10 @@ class CreateTweetScreen extends ConsumerStatefulWidget {
 }
 
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
+  final tweetTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final currentUSer = ref.watch(currentUserDetailsProvider).value;
-    print('2 ' + currentUSer.toString());
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -44,12 +46,62 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                     children: [
                       CircleAvatar(
                         backgroundImage: NetworkImage(currentUSer.profilePic),
+                        radius: 30,
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          onTapOutside: (_) {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                          },
+                          controller: tweetTextController,
+                          style: const TextStyle(fontSize: 22),
+                          decoration: const InputDecoration(
+                              hintText: "What's Happening?",
+                              hintStyle: TextStyle(
+                                  color: Pallete.greyColor,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600),
+                              border: InputBorder.none),
+                          maxLines: null,
+                        ),
                       )
                     ],
-                  )
+                  ),
                 ],
               ),
             )),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+            border: Border(top: BorderSide(color: Pallete.greyColor))),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 10, right: 10),
+              child: SvgPicture.asset(
+                AssetsConstants.galleryIcon,
+                height: 35,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 10, right: 10),
+              child: SvgPicture.asset(
+                AssetsConstants.gifIcon,
+                height: 35,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(left: 10, right: 15),
+              child: SvgPicture.asset(
+                AssetsConstants.emojiIcon,
+                height: 35,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
